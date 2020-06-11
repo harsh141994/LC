d7e1e7dc8e0a49d1377781786cb4ded75e2d9039
harshvarshney <harsh141994@gmail.com>
2020-06-11 13:19:28 -0400

    will cr pr

--- main.cpp
+++ main.cpp
@@ -4,17 +4,3 @@
 
 using namespace std;
 
-
-int main() {
-
-  vector<int> nums{-20, -10, 30};
-  vector<vector<int>> ans = threeSum(nums);
-
-  for (int i=0; i<ans.size(); i++) {
-    for (int j=0; j<ans[i].size(); j++) {
-      cout<<ans[i][j]<<" ";
-    }
-    cout<<endl;
-  }
-  return 0;
-}
